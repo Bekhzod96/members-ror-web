35b0fc947f953321d03089d1e1783109a81d633a
beka <mr.begzodjon96@mail.ru>
2020-06-03 18:50:50 +0300

    Username added to controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -41,13 +41,17 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def sign_up_params
+    params.require(:user).permit(:username, :email, :password, :confirmation_password)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:username, :email, :password, :confirmation_password, :current_password)
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+  #   devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :password, :password_confirmation])
   # end
 
   # The path used after sign up.
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,7 +2,10 @@
 
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-
+  def create
+    # custom sign-in code
+    redirect_to users_path 
+  end
   # GET /resource/sign_in
   # def new
   #   super
@@ -22,6 +25,6 @@ class Users::SessionsController < Devise::SessionsController
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:username, :email, :password, :password_confirmation])
   # end
 end
